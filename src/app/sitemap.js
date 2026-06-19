f83fb9ae4443ca5728ce9e4b82eee3cee35c4430
Mazen Al Swar <43959520+mazenswar@users.noreply.github.com>
2026-06-19 15:51:48 -0400

    v1.0.31 bug fix

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -1,4 +1,4 @@
-import site from "@/config/site";
+import site from "../../config/site";
 
 export default function sitemap() {
 	return [
@@ -12,25 +12,37 @@ export default function sitemap() {
 			url: `${site.url}/about`,
 			lastModified: new Date(),
 			changeFrequency: "monthly",
-			priority: 0.8,
+			priority: 0.7,
 		},
 		{
 			url: `${site.url}/services`,
 			lastModified: new Date(),
 			changeFrequency: "monthly",
-			priority: 0.8,
+			priority: 0.6,
 		},
 		{
-			url: `${site.url}/work`,
+			url: `${site.url}/case-studies/calm-therapy`,
 			lastModified: new Date(),
 			changeFrequency: "monthly",
 			priority: 0.7,
 		},
 		{
-			url: `${site.url}/contact`,
+			url: `${site.url}/websites`,
 			lastModified: new Date(),
 			changeFrequency: "yearly",
-			priority: 0.6,
+			priority: 0.8,
+		},
+		{
+			url: `${site.url}/audit`,
+			lastModified: new Date(),
+			changeFrequency: "yearly",
+			priority: 0.8,
+		},
+		{
+			url: `${site.url}/process`,
+			lastModified: new Date(),
+			changeFrequency: "yearly",
+			priority: 0.8,
 		},
 	];
 }
